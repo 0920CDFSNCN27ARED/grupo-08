8649dc7b0eeb228d7f12500e51c94e76293c0bb3
Leandro Muzzupappa <leann.muzzu@gmail.com>
2020-11-15 22:36:26 -0300

    Creo template slider productos
    Tambien se modifican las rutas de la ficha de producto para funcionar con express

--- site/public/scripts/main.js
+++ site/public/scripts/main.js
@@ -1,17 +1,21 @@
 function esMobile() {
-    return window.matchMedia("(max-width: 720px)").matches;
+    return window.matchMedia('(max-width: 720px)').matches;
 }
 function esTables() {
-    return window.matchMedia("(min-width: 721px) and (max-width: 999px)").matches;
+    return window.matchMedia('(min-width: 721px) and (max-width: 999px)').matches;
 }
 function esDesktop() {
-    return window.matchMedia("(min-width: 1000px)").matches;
+    return window.matchMedia('(min-width: 1000px)').matches;
 }
 
 // GALERIA DE PRODUCTOS
 const catalog_view = document.querySelector('.catalog_category_view');
 if (catalog_view) {
-    const getRandomID = () => Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+    const getRandomID = () =>
+        Math.random()
+            .toString(36)
+            .replace(/[^a-z]+/g, '')
+            .substr(0, 5);
 
     let productTemplate = (id) => `
         <article id="product_${id}" class="product">
@@ -80,12 +84,12 @@ if (catalog_view) {
     }
 
     let product_list = document.querySelectorAll('.product');
-    if(product_list.length > 0 || product_list !== undefined) {
-        product_list.forEach( product => {
+    if (product_list.length > 0 || product_list !== undefined) {
+        product_list.forEach((product) => {
             let swiper_photos_el = product.querySelector('.swiper-container');
 
-            if(swiper_photos_el) {
-                console.log( product.getAttribute('id') );
+            if (swiper_photos_el) {
+                console.log(product.getAttribute('id'));
                 let swiper_photos = new Swiper(swiper_photos_el, {
                     observer: true,
                     observeParents: true,
@@ -98,27 +102,20 @@ if (catalog_view) {
                     breakpoints: {
                         720: {
                             noSwiping: true,
-                        }
-                    }
+                        },
+                    },
                 });
-
             }
-
-        } )
-
+        });
     }
-    
-
-
-
 }
 
 // FICHA PRODUCTO
 const catalog_product_view = document.querySelector('.catalog_product_view');
-if(catalog_product_view) {
+if (catalog_product_view) {
     let swiper_photos_el = catalog_product_view.querySelector('.swiper-container');
 
-    let swiper_photos = new Swiper( swiper_photos_el, {
+    let swiper_photos = new Swiper(swiper_photos_el, {
         loop: true,
         pagination: {
             el: '.swiper-pagination',
@@ -131,7 +128,119 @@ if(catalog_product_view) {
                 slidesPerColumn: 10,
                 slidesPerColumnFill: 'row',
             },
-        }
+        },
+    });
+}
 
-    } )
+// SLIDER PRODUCTOS RECOMENDADOS
+const slider_featured_products = document.querySelector('.slider_featured_products');
+if (slider_featured_products) {
+    const product_template = `<div class="swiper-slide">
+        <article id="product_" class="product">
+            <div class="product_photos">
+                <a href="#link_to_product">
+                    <div class="swiper-container">
+                        <div class="swiper-wrapper">
+                            <div class="swiper-slide">
+                                <img
+                                    src="/images/catalog/category/img_producto-1.png"
+                                    alt="nombre_producto"
+                                />
+                            </div>
+                            <div class="swiper-slide">
+                                <img
+                                    src="/images/catalog/category/img_producto-2.png"
+                                    alt="nombre_producto"
+                                />
+                            </div>
+                            <div class="swiper-slide">
+                                <img
+                                    src="/images/catalog/category/img_producto-3.png"
+                                    alt="nombre_producto"
+                                />
+                            </div>
+                            <div class="swiper-slide">
+                                <img
+                                    src="/images/catalog/category/img_producto-4.png"
+                                    alt="nombre_producto"
+                                />
+                            </div>
+                            <div class="swiper-slide">
+                                <img
+                                    src="/images/catalog/category/img_producto-5.png"
+                                    alt="nombre_producto"
+                                />
+                            </div>
+                        </div>
+
+                        <div class="swiper_prev">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 10 9"
+                            >
+                                <g transform="translate(1098 -4084) rotate(90)">
+                                    <rect
+                                        class="a"
+                                        width="9"
+                                        height="10"
+                                        transform="translate(4084 1088)"
+                                    />
+                                    <path
+                                        class="b"
+                                        d="M3,4.5l3,3,3-3"
+                                        transform="translate(4082.5 1087)"
+                                    />
+                                </g>
+                            </svg>
+                        </div>
+                        <div class="swiper_next">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 10 9"
+                            >
+                                <g transform="translate(-1088 4093) rotate(-90)">
+                                    <rect
+                                        class="a"
+                                        width="9"
+                                        height="10"
+                                        transform="translate(4084 1088)"
+                                    />
+                                    <path
+                                        class="b"
+                                        d="M3,4.5l3,3,3-3"
+                                        transform="translate(4082.5 1087)"
+                                    />
+                                </g>
+                            </svg>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="product_info">
+                <a href="#link_to_product">
+                    <h3>Producto 1</h3>
+                    <p>$2.835</p>
+                </a>
+            </div>
+        </article>
+    </div>`;
+
+    const products_wrapper = slider_featured_products.querySelector('.swiper-wrapper');
+    products_wrapper.innerHTML += product_template.repeat(6);
+
+    let swiper_photos_el = slider_featured_products.querySelector('.swiper-container');
+    let swiper_photos = new Swiper(swiper_photos_el, {
+        slidesPerView: 1,
+        loop: true,
+        grabCursor: true,
+        spaceBetween: 32,
+        breakpoints: {
+            500: {
+                slidesPerView: 2,
+            },
+            1000: {
+                slidesPerView: 4,
+            },
+        },
+    });
 }
