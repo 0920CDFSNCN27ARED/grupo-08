bb63685888ff5aa0a59ea091864ebb6694a6daaa
CantonLeandro <cantonleandro@gmail.com>
2020-11-20 23:06:43 -0300

    Agrego pagina de registro

--- site/routes/mainRoutes.js
+++ site/routes/mainRoutes.js
@@ -2,14 +2,14 @@ const express = require("express");
 const routes = express.Router();
 
 // Controllers
-const mainControllers = require("../controllers/mainControllers")
+const mainControllers = require("../controllers/mainControllers");
 
 routes.get("/", mainControllers.inicio);
-routes.get("/galeria", mainControllers.galeria_productos)
-routes.get("/ficha", mainControllers.ficha_producto)
-routes.get("/login", mainControllers.login)
+routes.get("/galeria", mainControllers.galeria_productos);
+routes.get("/ficha", mainControllers.ficha_producto);
+routes.get("/login", mainControllers.login);
+routes.get("/register", mainControllers.registro);
 
 routes.get("*", mainControllers.notFound);
-
 
 module.exports = routes;
