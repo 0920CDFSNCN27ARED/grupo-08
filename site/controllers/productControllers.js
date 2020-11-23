64ffb6948f2d40433bc8733075b64b204ff5be5f
Leandro Muzzupappa <leann.muzzu@gmail.com>
2020-11-23 15:51:56 -0300

    tst commit

--- site/controllers/productControllers.js
+++ site/controllers/productControllers.js
@@ -1,6 +1,12 @@
+const galleryOptions = {
+    pageTitle: 'Nombre galeria',
+    pageDescription:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora nulla perferendis quae soluta reprehenderit fugit, quasi harum at ratione, illo, nisi dolor molestiae! Incidunt fugit id asperiores vitae totam officiis?',
+};
+
 const productControllers = {
     gallery: (req, res) => {
-        res.render('pages/products_gallery');
+        res.render('pages/products_gallery', galleryOptions);
     },
 
     details: (req, res) => {
