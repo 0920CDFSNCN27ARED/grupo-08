fb2da04a3a58c729f7a5f619dacf1a1858e4cf39
CantonLeandro <cantonleandro@gmail.com>
2020-12-27 20:49:40 -0300

    creo jsons y algunas rutas de prductos

--- site/controllers/productControllers.js
+++ site/controllers/productControllers.js
@@ -12,6 +12,49 @@ const productControllers = {
     details: (req, res) => {
         res.render('pages/product_details');
     },
+
+    showCreate: (req, res) => {
+        res.render('pages/product_create');
+    },
+
+    create: (req, res, next) => {
+        const products = getProducts();
+        const lastProductIndex = products.length - 1;
+        const lastProduct = products[lastProductIndex];
+        const newId = lastProduct.product_id + 1;
+        const images = [];
+        for (let i = 0; i < req.files.length; i++) {
+            req.files[i].filename.push(images);
+        }
+
+        const product = {
+            product_id: newId,
+            title: req.body.title,
+            colors: req.body.colors,
+            category: req.body.category,
+            subcategory: req.body.subcategory,
+            price: req.body.price,
+            discount: req.body.discount,
+            images: images,
+            sizes: req.body.sizes,
+        };
+
+        saveProducts(products.push(product));
+
+        res.redirect(`pages/c/producto/${newId}`);
+    },
+
+    showEdit: (req, res) => {
+        res.render('pages/product_edit');
+    },
+
+    edit: (req, res) => {
+        res.render('pages/product_edit');
+    },
+
+    delete: (req, res) => {
+        res.render('pages/product_edit');
+    },
 };
 
 module.exports = productControllers;
