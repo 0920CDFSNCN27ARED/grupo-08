478a9c1fa5960d6b973803d08823d4e8e2bb8a33
Leandro Muzzupappa <leann.muzzu@gmail.com>
2021-01-03 23:12:46 -0300

    Se termina la pagina de listar productos

--- site/routes/admin/catalogRoutes.js
+++ site/routes/admin/catalogRoutes.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const routes = express.Router();
 
-routes.get('/productos/:producto?/:pid', (req, res) => {
-    res.send('productos');
+routes.get('/productos/', (req, res) => {
+    res.render('admin/pages/products-list');
 });
 
-routes.get('/producto/crear', (req, res) => {
-    res.render('admin/pages/product-create');
+routes.get('/productos/crear', (req, res) => {
+    res.render('products-list');
 });
 
-routes.get('/categorias/:categoria?/id/:catid', (req, res) => {
-    res.send('categoria');
+routes.get('/productos/:id', (req, res) => {
+    res.render('products-list');
 });
 
-routes.get('/categoria/crear', (req, res) => {
-    res.send('crear categoria');
+routes.get('/productos/:id/edit', (req, res) => {
+    res.render('products-list');
 });
 
 module.exports = routes;
