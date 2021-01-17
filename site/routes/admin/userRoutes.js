37151c9eb7832f9d902b5c060465036461a36c9e
leandromuzzupappa <leann.muzzu@gmail.com>
2021-01-17 05:17:24 -0300

    Se creo el crud de administradores
    Queda pendiente crear coockies, sesion y validar si el usuario quiere mantener la sesion o no

--- site/routes/admin/userRoutes.js
+++ site/routes/admin/userRoutes.js
@@ -4,5 +4,12 @@ const routes = express.Router();
 const userControllers = require('../../controllers/admin/userControllers');
 
 routes.get('/login', userControllers.login);
+routes.post('/login', userControllers.logged);
+
+routes.get('/register', userControllers.register);
+routes.post('/register', userControllers.create);
+
+
+
 
 module.exports = routes;
